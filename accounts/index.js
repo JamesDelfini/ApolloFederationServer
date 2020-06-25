6d2e4e4f545bc9c750d5ff1e55d16777d69f319f
Dennis James Matildo <dj.matildo@gmail.com>
2020-06-25 11:32:56 +0800

    Add JWT-based Authentication with Express Middleware

--- accounts/index.js
+++ accounts/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { buildFederatedSchema } = require("@apollo/federation");
+const jwt = require("jsonwebtoken");
 
 const { accounts } = require("./data");
 
@@ -13,6 +14,11 @@ const typeDefs = gql`
   extend type Query {
     account(id: ID!): Account
     accounts: [Account]
+    viewer: Account!
+  }
+
+  extend type Mutation {
+    login(email: String!, password: String!): String
   }
 `;
 
@@ -28,12 +34,31 @@ const resolvers = {
     },
     accounts() {
       return accounts;
+    },
+    viewer(_, { id }, { user }) {
+      return accounts.find(account => account.id === user.sub);
+    }
+  },
+  Mutation: {
+    login(_, { email, password }) {
+      const { id, permissions, roles } = accounts.find(
+        account => account.email === email && account.password === password
+      );
+      return jwt.sign(
+        { "https://awesomeapi.com/graphql": { roles, permissions } },
+        "f1BtnWgD3VKY",
+        { algorithm: "HS256", subject: id, expiresIn: "1d" }
+      );
     }
   }
 };
 
 const server = new ApolloServer({
-  schema: buildFederatedSchema([{ typeDefs, resolvers }])
+  schema: buildFederatedSchema([{ typeDefs, resolvers }]),
+  context: ({ req }) => {
+    const user = req.headers.user ? JSON.parse(req.headers.user) : null;
+    return { user };
+  }
 });
 
 server.listen({ port }).then(({ url }) => {
